f43db12796c064ec08f5e22935f25cc6a3e06f2c
afriza <afrizahelmi35@gmail.com>
2018-04-08 06:23:25 +0800

    Getter and Setter

--- src/praktek3/KapalTitanic.java
+++ src/praktek3/KapalTitanic.java
@@ -6,5 +6,15 @@ public class KapalTitanic extends Kapal {
     void throttle(){
         System.out.println("Nyoossssss.....");
     }
+
+    public String getJenisKapal() {
+        return jenisKapal;
+    }
+
+    public void setJenisKapal(String jenisKapal) {
+        this.jenisKapal = jenisKapal;
+    }
+    
+    
     
 }
