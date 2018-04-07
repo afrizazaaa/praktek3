879fa77d4bbf0e132e8c695db54bbaaa1eac04e4
afriza <afrizahelmi35@gmail.com>
2018-04-08 05:41:10 +0800

    Membuat method cetakInfo{

--- src/praktek3/Kapal.java
+++ src/praktek3/Kapal.java
@@ -3,5 +3,11 @@ package praktek3;
 public class Kapal {
    String kargo;
    String jenis;
-   Double harga;
+   double harga;
+   
+   void cetakInfo(){
+       System.out.println("Kargo \t: "+kargo+"\n"+
+               "Jenis \t: "+jenis+"\n"+
+               "Harga \t:"+harga);
+   }
 }
