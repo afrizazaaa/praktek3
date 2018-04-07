bd3c926f1dc38a72f5d75f9dd2100b80556cf07e
afriza <afrizahelmi35@gmail.com>
2018-04-08 06:06:32 +0800

    menggunakan method set

--- src/praktek3/kapalaksi.java
+++ src/praktek3/kapalaksi.java
@@ -3,9 +3,9 @@ package praktek3;
 public class kapalaksi {
     public static void main(String[] args) {
         Kapal q = new Kapal ();
-        q.kargo="3";
-        q.jenis="Titanic";
-        q.harga=3000000;
+        q.setKargo("3");
+        q.setJenis("Titanic");
+        q.setHarga(3000000);
         
         q.cetakInfo();
     }
