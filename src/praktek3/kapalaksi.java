6e36368acf6aa8be3f0598a0abb8475df489cd42
afriza <afrizahelmi35@gmail.com>
2018-04-08 05:54:07 +0800

    Instance buat method dan panggil method

--- src/praktek3/kapalaksi.java
+++ src/praktek3/kapalaksi.java
@@ -2,6 +2,11 @@ package praktek3;
 
 public class kapalaksi {
     public static void main(String[] args) {
+        Kapal q = new Kapal ();
+        q.kargo="3";
+        q.jenis="Titanic";
+        q.harga=3000000;
         
+        q.cetakInfo();
     }
 }
