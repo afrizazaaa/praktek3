64ce97304c9871bf2362f7fa9869173ead3f3c30
afriza <afrizahelmi35@gmail.com>
2018-04-08 06:21:56 +0800

    Atribute dan method

--- src/praktek3/KapalTitanic.java
+++ src/praktek3/KapalTitanic.java
@@ -1,5 +1,10 @@
 package praktek3;
 
 public class KapalTitanic extends Kapal {
+    private String jenisKapal;
+    
+    void throttle(){
+        System.out.println("Nyoossssss.....");
+    }
     
 }
